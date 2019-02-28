705ed24f3dd4572703e0ecbf2e68466bc9cb1661
PatrickWumbo <jhuh009@ucr.edu>
2019-02-27 23:21:04 -0800

    simpleRSA component

--- simpleRSA.c
+++ simpleRSA.c
@@ -3,7 +3,7 @@
 
 //create GCD function
 unsigned long int GCD(unsigned long int a, unsigned long int b){
-    int c;
+    unsigned long int c;
     while(1){
         c = a % b;
         if(c==0) return b;
@@ -15,8 +15,8 @@ unsigned long int GCD(unsigned long int a, unsigned long int b){
 
 int main(){
     //rsa requires a p, q, n, e, d, phi;
-    double p = 11;
-    double q = 17;
+    double p = 3;
+    double q = 7;
     double n = p*q;
     double phi = (p-1)*(q-1);
 
@@ -32,27 +32,32 @@ int main(){
     
     // generating private key
     //decrypt key d
-    double d;
+    unsigned long int d;
 
     //k 
     double k = 2;
-
-    //ensure that the decryption key satifies the check
-    d = (1+(k*phi))/e;
     double msg = 12; // length
-    double c = pow(msg,e);// encrypt the message with the encryption key
-    c = fmod(c,n);
-    double m = pow(c,d); // decrypt the message
-    m = fmod(m,n);
-
     printf("Message data = %lf",msg);
     printf("\np = %lf",p);
     printf("\nq = %lf",q);
     printf("\nn = pq = %lf",n);
     printf("\nphi = %lf",phi);
     printf("\ne = %lf",e);
+   
+
+    //ensure that the decryption key satifies the check
+    d = (1+(k*phi))/e;
     printf("\nd = %lf",d);
+    double c = pow(msg,e);// encrypt the message with the encryption key
+    c = fmod(c,n);
     printf("\nEncrypted data = %lf",c);
+    double m = pow(c,d); // decrypt the message
+    m = fmod(m,n);
     printf("\nOriginal Message Sent = %lf",m);
+
+    
+    
+    
+    
     return 0;
 }
